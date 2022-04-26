ec2dba4dbf4c48c675e0e746949bf81fe51d9787
Kajetan Wiacek <kajetanw49@gmail.com>
2022-04-26 23:16:13 +0200

    Initial commit

---
+++ src/main/java/com/kajetanwiacek/concerttickets/ConcertTicketsApplication.java
@@ -0,0 +1,11 @@
+package com.kajetanwiacek.concerttickets;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertTicketsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kajetanwiacek/concerttickets/ConcertTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kajetanwiacek.concerttickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
